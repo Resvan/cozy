2f70a501dbfbcff49f512a7ceb7f186687f50e3b
sambhubaburaj <sambhubaburaj513@gmail.com>
2024-01-08 23:20:39 +0530

    some changes were made

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,144 +1,40 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import styles from"../../../public/style/style.css";
+"use client";
+import * as React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import styles from "../../../public/style/style.css";
 import "../../../public/style/style.css";
-import { useSpring, animated } from 'react-spring';
-import moon  from  '../../../public/images/moon.png'
-const TextAnimation = () => {
-
-
-
-
-
-  const [mouseX, setMouseX] = useState(0);
-  const [maxTranslation, setMaxTranslation] = useState(0);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setMaxTranslation(window.innerWidth / 2);
-    };
-
-    // Initial calculation and add event listener for resizing
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const calculateTranslation = (value) => {
-    const restrictedValue = Math.min(Math.max(value, -maxTranslation+500), maxTranslation-500);
-    return `${restrictedValue}px`;
-  };
-
-  const leftTextSpring = useSpring({
-    transform: `translateX(${calculateTranslation(mouseX + 50)})`,
-  });
-
-  const rightTextSpring = useSpring({
-    transform: `translateX(${calculateTranslation(-mouseX - 50)})`,
-  });
-  const alignCenter = { display: 'flex', alignItems: 'center' }
-
+import background from "../../../public/images/bird_2.jpg";
+export default function App() {
+  const alignCenter = { display: "flex", alignItems: "center" };
   return (
-    
-<>
-   
-
-
-    <div
-      onMouseMove={(e) => setMouseX(e.clientX)}
-      style={{
-        background:'red',
-        position: '',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <animated.div className="text-8xl font-bold uppercase" style={rightTextSpring}>Reinvent What Your </animated.div>
-      <animated.div className="text-8xl font-bold uppercase" style={leftTextSpring}>Business Could Be</animated.div>
-    </div>
-
-<div>
-<Parallax className=''   pages={5}>
-        <ParallaxLayer style={{
-          
-          backgroundImage:`url(${moon})`,
-      backgroundSize:"cover"
-      }} offset={0} speed={0.5}
-      
-      factor={2}>
-        
-        </ParallaxLayer>
-
-        <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ }}>
-          <div className={`${styles.card} ${styles.sticky}`}>
-            <p>m a sticky layer</p>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.5} speed={1.5} style={{ }}>
-         
-
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
-
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: '' }}>
-                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-
-
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
-
-        </ParallaxLayer>
-      </Parallax>
-
-
-</div>
-<div className='h-96 '>
-  woifwersf
-</div>
-
-<section class="parallax__group">
+    <>
+      Z<div className="h-96">fgwef</div>
+      <div class="header">
+        <h1>Pure CSS Parallax, Y-Axis pseudo-3D</h1>
+        <p>
+          Parallax effect experiment #4: minimal code example inspired by{" "}
+          <a href="https://keithclark.co.uk/articles/pure-css-parallax-websites/">
+            Keith Clark
+          </a>{" "}
+          and{" "}
+          <a href="https://codepen.io/hexagoncircle/pen/JjRYaZw">
+            Ryan Mulligan
+          </a>
+          , explained at{" "}
+          <a href="https://dev.to/ingosteinke/pure-css-parallax-perspective-beyond-landscape-images-24g2">
+            dev.to/ingosteinke/pure-css-parallax-perspective-beyond-landscape-images-24g2
+          </a>{" "}
+          using only 2D Y-axis transformations, no global body styles, no
+          misleading clutter, and no JavaScript at all.
+        </p>
+      </div>
+      <p class="content">
+        Side effect: preceding paragraphs must define a non-transparent
+        background, otherwise the decoration will shine through. <b>Scaling:</b>{" "}
+        this setup will upscale your content, possibly breaking pixel-perfect
+        designs!
+      </p>
+      <section class="parallax__group">
         <div class="parallax__layer parallax__layer--background"></div>
         <div class="parallax__layer parallax__layer--foreground">
           <div className="p-5">
@@ -445,27 +341,6 @@ const TextAnimation = () => {
         </div>
         {/* <!-- more layers could be added here --> */}
       </section>
-
-      <div
-      onMouseMove={(e) => setMouseX(e.clientX)}
-      style={{
-        background:'red',
-        position: '',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <animated.div className="text-8xl font-bold uppercase" style={rightTextSpring}>Reinvent What Your </animated.div>
-      <animated.div className="text-8xl font-bold uppercase" style={leftTextSpring}>Business Could Be</animated.div>
-    </div>
-<div  className='h-96'>
-f
-</div>
-</>
+    </>
   );
-};
-
-export default TextAnimation;
+}
