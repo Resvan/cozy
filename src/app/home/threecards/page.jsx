48331c94d37b7f579aa51faded5d1045ab380808
sambhubaburaj <sambhubaburaj513@gmail.com>
2024-01-10 09:36:02 +0530

    transision card was created

---
+++ src/app/home/threecards/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from "react";
+import "./style.css"
+import { motion, useAnimation } from "framer-motion";
+import SlideCards1 from "@/app/components/SlidingCards/SlideCards1";
+
+export default function Page() {
+ return(<>
+ 
+<SlideCards1/>
+ </>)
+}
